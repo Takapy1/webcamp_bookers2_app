9aa70cd5dca111c047754c1d5fa4cc36bd8cf1c0
Takapy1 <takahirog1109mai@gmail.com>
2021-03-15 23:50:31 +0000

    [Finish] header and footer in login, logout page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
+  get '/about' => 'homes#about'
+  resources :users, only: [:index, :show]
+  resources :books, only: [:index, :create, :edit, :update, :destroy]
 end
